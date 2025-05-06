a8ee4852bbfce616378b6619b37f571fbe22894e
Ethan P. Reker <ethan.reker@wwinc.com>
2025-05-05 21:35:36 -0500

    increment version to 0.16-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.abaga129.tekisuto"
         minSdk = 30 // Updated to API 30 (Android 11) for screenshot functionality
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.15-alpha"
+        versionCode = 16
+        versionName = "0.16-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
